40198fe1a46cf9e7da90452e158dbb02c141a3e0
Nazar Mosula <nmosula@gmail.com>
2023-05-03 21:08:34 +0300

    added invokeAction

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("First project by Node.js");
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      return console.table(contactsList);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+
+    case "add":
+      const addContact = await contacts.addContact({ name, email, phone });
+      return console.table(addContact);
+
+    case "remove":
+        const removeContact = await contacts.removeContact(id);
+        return console.table(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
